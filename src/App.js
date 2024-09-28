0248019e12cd5ec8f072db47bfb76b2ff164d6b1
Hema Nalawade <98449213+HemaNalawade@users.noreply.github.com>
2024-09-29 00:29:55 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import React from 'react';
+import NavRoutes from './components/user/NavRoutes';
+
+
+
+
+function App() {
+  return (
+     <div>
+    <NavRoutes/>
+    
+    </div>
+    
+    
+  );
+}
+
+export default App;
